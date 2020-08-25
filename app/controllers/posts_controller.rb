013dbb473d01cd4f675dd05456ddc08b699861e4
daisuke sasaki <gerund25495641@gmail.com>
2020-08-25 19:25:14 +0900

    画像投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
     def index
       @posts = Post.all
+      @post = Post.new
     end
 
     def new
@@ -10,6 +11,11 @@ class PostsController < ApplicationController
 
     def create
       @post = Post.new(post_params)
+      if @post.save
+        redirect_back(fallback_location: root_path)
+      else
+        redirect_back(fallback_location: root_path)
+      end
     end
 
     private
