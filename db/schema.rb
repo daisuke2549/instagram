bf340cb4830f694b71a9f33d27d2ee60c6c58a59
daisuke sasaki <gerund25495641@gmail.com>
2020-08-30 09:28:07 +0900

    deviseを再インストール

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_102634) do
+ActiveRecord::Schema.define(version: 2020_08_30_002700) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -63,10 +63,24 @@ ActiveRecord::Schema.define(version: 2020_08_24_102634) do
     t.bigint "post_id"
   end
 
+  create_table "models", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_models_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+  end
+
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "content"
+    t.string "img"
+    t.text "title"
   end
 
   create_table "profiles", force: :cascade do |t|
