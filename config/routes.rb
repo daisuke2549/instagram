72df24ac616170a2e5af8c02e6553b4483233fc6
daisuke sasaki <gerund25495641@gmail.com>
2020-09-07 19:12:50 +0900

    最新のコードをコミット

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> image_post
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "profiles#index"
   resources :posts do
