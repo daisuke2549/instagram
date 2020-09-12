c094966e137ccfc7b739c1b189e2c76abf614a58
daisuke sasaki <gerund25495641@gmail.com>
2020-09-13 08:00:59 +0900

    no message

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
   devise_for :accounts
   devise_scope :account do
     get '/accounts/sign_out' => 'devise/sessions#destroy'
   end
-=======
->>>>>>> master
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "profiles#index"
   resources :posts do
