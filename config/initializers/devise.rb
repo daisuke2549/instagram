a5eeb81693fb7fc5d0d50acbe1359f2ded9f3ed0
daisuke sasaki <gerund25495641@gmail.com>
2020-09-21 11:59:24 +0900

    deviseでログイン画面を実装しUIについてはほぼ完成

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -242,7 +242,7 @@ Devise.setup do |config|
 
   # ==> Scopes configuration
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
-  # "users/sessions/new". It's turned off by default because it's slower if you
+  # "accounts/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
   # config.scoped_views = false
 
